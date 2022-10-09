ac1e661fa8b58ed6c4118a47ab3c56af124343c7
RealBigHindi <max.neuber@googlemail.com>
2022-10-10 01:48:11 +0200

    made storage to be async to midigate storage creation error

--- modules/state/persistedReducer.tsx
+++ modules/state/persistedReducer.tsx
@@ -1,10 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import reducers from "./reducers";
 
 const persistConfig = {
     key: 'root',
-    storage,
+    storage: AsyncStorage,
   }
-  
+
   export default persistReducer(persistConfig, reducers)
